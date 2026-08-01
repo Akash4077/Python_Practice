492934c965ea41ffa561837e14c9ecac8cbb9538
Akash Kumar Mandal <hunterboy9778@gmail.com>
2026-08-01 14:54:29 +0530

    Add files via upload

---
+++ Exception handling.py
@@ -0,0 +1,7 @@
+#Exception handling
+a=input("Enter the number:")
+print(f"Multiplication table of {a} is:")
+for i in range(1,11):
+    print(f"{int(a)}*{i}={int(a)*i}")
+print("Some imp line of code")
+print("End of program")
